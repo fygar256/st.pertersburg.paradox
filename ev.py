96e0c81394167b9a9675a09818f5c12a1fe0ac00
FYGAR256 <fygar256@gmail.com>
2023-02-20 09:46:47 +0900

    Add files via upload

--- ev.py
+++ ev.py
@@ -2,9 +2,7 @@
 import os
 import binascii
 import random
-import mpmath
 from getkey import getkey,keys
-mpmath.mp.dps=1000
 
 #方法１
 # ∞＝１００００とした場合のSの近似値
@@ -12,9 +10,8 @@ mpmath.mp.dps=1000
 #
 s=0
 for i in range(10000):
- s=s+1/mpmath.power(2,i)
-print("方法１:",end='')
-print(2**(s-1))
+ s=s+1/2**i
+print("方法１:",2**(s-1))
 
 print("Hit any key")
 key=getkey()
@@ -23,13 +20,14 @@ key=getkey()
 s2=0
 N=10000
 l=[]
+r=[]
 f=open("/dev/random",'rb') # /dev/randomを開く
 random32bitdata=f.read(4) # ４バイト読み出し
 randomhex=binascii.hexlify(random32bitdata) #１６進の文字列に変換
 randomint=int(randomhex,16) # 整数に変換
 random.seed(randomint) # ランダムシードを初期化
 
-# ｎ回ゲームをした場合の結果のリストを作る
+# ｎ回シミュレーションをした場合の結果のリストを作る
 for i in range(N):
  k=0
  while(True):
@@ -37,14 +35,21 @@ for i in range(N):
    coin=random.randrange(2)
    if coin==0:
      break
- l+=[float(mpmath.power(2,k-1))]
+ r+=[k]
+ l+=[2**(k-1)]
 
 # 平均を取る
 s=0
+t=0
 for i in range(len(l)):
  s+=l[i]
+ t+=r[i]
 a=s/N
+b=t/N
+
 print("シミュレーション")
-print(l)
-print("平均: ",end='')
-print(a)
+print("回数：",r)
+print("利得額：",l)
+print("コインを投げる平均回数: ",b)
+print("μ(%d)=%f"%(N,2**(b-1)))
+print("平均利得額: ",a)
